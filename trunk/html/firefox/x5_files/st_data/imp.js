f069c9fae5f945be07824d6bdb609bca1446aa93
lastdm@gmail.com <lastdm@gmail.com@ce445602-59e8-3421-48bf-c22d16e2488a>
2012-01-15 16:26:32 +0000

    Добавлен пример заезда в х5

---
+++ trunk/html/firefox/x5_files/st_data/imp.js
@@ -0,0 +1,6 @@
+document.write('<a target=\"_blank\" href=\"http://ad.yieldmanager.com/clk?3,eAGlkN1OwzAMhZ-mdwUltUc2RVy46yoKW0q0iNHdJRsr.RmtoKDy9nSs4wWwLNnHn3SOZA5ywi2wmQiE21uHOy454EvArXN48JmUMhD8BlCwKfPpVCFcPdA8b53-lZQ-2.txJYoSoux8p4QiTeEC08PjyPWk6Uf4zzGv0.4SOkTmZ7s78WGx1OGfeaQh2zyVqtS4NOqYmqRbmbherRlmm221NDvcGlUos-hVGRbZxZLo1vdfu671gLwgHrqq7VeTN29Vcf3-Oei2tt8nAnF-LPYeRMMLZ9Mfl4heBw==,\"><img border=\"0\" alt=\"\" height=\"90\" width=\"728\" src=\"http://content.yieldmanager.edgesuite.net/atoms/ed/0c/b7/07/ed0cb707f18040ff4346ddb415bdd42f.jpg\"></img></a>');
+var rm_data = new Object();
+rm_data.creative_id = 11838880;
+rm_data.offer_type = 20;
+rm_data.entity_id = 384665;
+if (window.rm_crex_data) {rm_crex_data.push(11838880);}
