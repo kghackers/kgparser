4b5f6b339a3b4308d67d334123977208f7293cbc
lastdm <lastdm@gmail.com>
2012-01-15 16:26:32 +0000

    Добавлен пример заезда в х5

---
+++ html/firefox/x5_files/st_data_002/imp.js
@@ -0,0 +1,6 @@
+document.write('<a target=\"_blank\" href=\"http://ad.yieldmanager.com/clk?3,eAGljdtuwjAMQL-mb92UxBEtivbgNlSLVtpVC0PlLaHQtQTKoJvY349L2Q.MsmQf2zqmIJaGUFKtg5FdsYrZSlDgK0aNtRx8IoRgAR0BD0hIfLxEBA8vGLNPfiXE53An66FHqTDuBlAoC4wmPF-.FreRrXF2v.xfjV1-GqSoEIf.Kj0a3hbRn1sWUM7fXaldm-psm2vVT3Xipm-El.OFS.WSL3TWZHpyytqoKe9KxCff.-j7vQfoseScG2e-u7rbbZrHw9eZ9878XDaQ1Num8kCOAzYOfwG9Jl5O,\"><img border=\"0\" alt=\"\" height=\"400\" width=\"240\" src=\"http://content.yieldmanager.edgesuite.net/atoms/b7/6c/a0/05/b76ca005140dc6b52eeb5de5b903f33e.jpg\"></img></a>');
+var rm_data = new Object();
+rm_data.creative_id = 8432566;
+rm_data.offer_type = 4;
+rm_data.entity_id = 216628;
+if (window.rm_crex_data) {rm_crex_data.push(8432566);}
