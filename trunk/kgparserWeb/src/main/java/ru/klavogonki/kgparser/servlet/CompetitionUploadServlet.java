a7a0c90ff90bba9691d9d55f095cad25d587785e
dmitry.weirdo@gmail.com <dmitry.weirdo@gmail.com@ce445602-59e8-3421-48bf-c22d16e2488a>
2014-02-13 15:35:23 +0000

    Добавлен черновик competitionUpload-сервлета. Страница competitionUpload.html перенесена в веб-модуль.

--- trunk/kgparserWeb/src/main/java/ru/klavogonki/kgparser/servlet/CompetitionUploadServlet.java
+++ trunk/kgparserWeb/src/main/java/ru/klavogonki/kgparser/servlet/CompetitionUploadServlet.java
@@ -1,10 +1,19 @@
 package ru.klavogonki.kgparser.servlet;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import ru.klavogonki.kgparser.entity.CompetitionEntity;
 import ru.klavogonki.kgparser.entity.CompetitionEntityService;
+import su.opencode.kefir.util.StringUtils;
 import su.opencode.kefir.web.Action;
 import su.opencode.kefir.web.InitiableAction;
 import su.opencode.kefir.web.JsonServlet;
+
+import java.io.UnsupportedEncodingException;
+import java.util.List;
 
 /**
  * Copyright 2014 <a href="mailto:dmitry.weirdo@gmail.com">Dmitriy Popov</a>.
@@ -21,8 +30,26 @@ public class CompetitionUploadServlet extends JsonServlet
 		{
 			@Override
 			public void doAction() throws Exception {
+				CompetitionEntity entity = new CompetitionEntity();
+
+				if (ServletFileUpload.isMultipartContent(request))
+				{
+					entity = processMultipartContent();
+				}
+				else
+				{
+					// todo: return error normally
+					response.getOutputStream().write( "Необходимо загрузить файл".getBytes(StringUtils.CHARSET_UTF8) );
+					return;
+				}
+
+				// todo: validate entity fields
+
+				// todo: parse zip file and fill competitionJson
+
 				CompetitionEntityService service = getService(CompetitionEntityService.class);
 
+/*
 				CompetitionEntity entity = new CompetitionEntity();
 				entity.setName("Название соревнования");
 				entity.setLink("http://klavogonki.ru");
@@ -34,10 +61,59 @@ public class CompetitionUploadServlet extends JsonServlet
 				entity.setZipFileContentType("application/zip");
 
 				entity.setCompetitionJson("{\"someField\": \"some value\"}");
+*/
 
 				service.createCompetitionEntity(entity);
 				writeSuccess();
 			}
+
+			private CompetitionEntity processMultipartContent() throws FileUploadException, UnsupportedEncodingException {
+				CompetitionEntity entity = new CompetitionEntity();
+
+				FileItemFactory factory = new DiskFileItemFactory();
+//				factory.setSizeThreshold(BUF_SIZE);
+//				factory.setRepository(new File(TEMP_DIRECTORY));
+
+				ServletFileUpload upload = new ServletFileUpload(factory);
+				List<FileItem> items = upload.parseRequest(request);
+
+				for (FileItem item : items)
+				{
+					String fieldName = item.getFieldName();
+
+					if (item.isFormField())
+					{ // simple field
+						String itemStr = item.getString(StringUtils.CHARSET_UTF8);
+
+						switch (fieldName)
+						{
+							case NAME_FILE_FIELD_NAME: entity.setName(itemStr); break;
+							case LINK_FILE_FIELD_NAME: entity.setLink(itemStr); break;
+							case COMMENT_FILE_FIELD_NAME: entity.setComment(itemStr); break;
+						}
+					}
+					else
+					{ // file field
+						if (fieldName.equals(ZIP_FILE_FIELD_NAME))
+						{
+							if (item.getSize() > 0)
+							{ // prevent parsing empty file fields
+								entity.setZipFileData( item.get() );
+								entity.setZipFileName(item.getName());
+								entity.setZipFileSize(item.getSize());
+								entity.setZipFileContentType(item.getContentType());
+							}
+						}
+					}
+				}
+
+				return entity;
+			}
 		};
 	}
+
+	public static final String NAME_FILE_FIELD_NAME = "name";
+	public static final String LINK_FILE_FIELD_NAME = "link";
+	public static final String COMMENT_FILE_FIELD_NAME = "comment";
+	public static final String ZIP_FILE_FIELD_NAME = "zipFile";
 }
