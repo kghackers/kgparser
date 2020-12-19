4951e4d1fbba50dd4b6bcb2327351af2b4b63330
Oleksandr Dievri <o.dievri@gmail.com>
2020-12-19 03:45:26 +0200

    Test commit

--- kgparserSpringBoot/src/test/java/ru/klavogonki/kgparser/springboot/KgParserApplicationTests.java
+++ kgparserSpringBoot/src/test/java/ru/klavogonki/kgparser/springboot/KgParserApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @Disabled
 @SpringBootTest
 class KgParserApplicationTests {
