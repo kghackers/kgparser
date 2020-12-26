b45da4027ef1f534e362a2e27882661a6732447f
dmitry-weirdo <dmitry.weirdo@gmail.com>
2020-12-26 21:31:09 +0100

    feat: #26 Added a PlayerSummaryDownloader method that splits the given players range into chunks of threadsCount size and tests for it.

--- kgparserSrv/src/main/java/ru/klavogonki/kgparser/PlayerSummaryDownloader.java
+++ kgparserSrv/src/main/java/ru/klavogonki/kgparser/PlayerSummaryDownloader.java
@@ -1,6 +1,7 @@
 package ru.klavogonki.kgparser;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import ru.klavogonki.kgparser.http.UrlConstructor;
@@ -11,6 +12,9 @@ import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class PlayerSummaryDownloader {
@@ -71,13 +75,24 @@ public class PlayerSummaryDownloader {
 
         Config config = Config.parseFromArguments(args);
 
-        config.setStartDate(DateUtils.formatDateTime(LocalDateTime.now()));
+        LocalDateTime startDate = LocalDateTime.now();
+        logger.info("Download start date: {}", startDate);
+
+        config.setStartDate(DateUtils.formatDateTime(startDate));
         config.log();
 
         for (int playerId = config.minPlayerId; playerId <= config.maxPlayerId; playerId++) {
             savePlayerSummaryToJsonFile(config, playerId);
             savePlayerIndexDataToJsonFile(config, playerId);
         }
+
+        LocalDateTime endDate = LocalDateTime.now();
+        logger.info("Download end date: {}", startDate);
+
+        logger.info("Downloading data for {} players took:", config.maxPlayerId - config.minPlayerId + 1);
+        logger.info("Hours: {}", ChronoUnit.HOURS.between(startDate, endDate));
+        logger.info("Minutes: {}", ChronoUnit.MINUTES.between(startDate, endDate));
+        logger.info("Seconds: {}", ChronoUnit.SECONDS.between(startDate, endDate));
     }
 
     private static void savePlayerSummaryToJsonFile(final Config config, final int playerId) throws IOException {
@@ -116,5 +131,44 @@ public class PlayerSummaryDownloader {
         logger.debug("Response for url {}:", urlString);
         logger.debug(out);
         return out;
+    }
+
+    // returns list of minPlayerId; maxPlayerId
+    static List<ImmutablePair<Integer, Integer>> split(int threadsCount, int minPlayerId, int maxPlayerId) {
+        if (minPlayerId > maxPlayerId) {
+            throw new IllegalArgumentException(String.format("minPlayerId = %d > maxPlayerId = %d", minPlayerId, maxPlayerId));
+        }
+
+        int totalPlayers = maxPlayerId - minPlayerId + 1;
+        int chunkSize = totalPlayers / threadsCount;
+
+        logger.debug("threadsCount: {}", threadsCount);
+        logger.debug("minPlayerId: {}", minPlayerId);
+        logger.debug("maxPlayerId: {}", maxPlayerId);
+        logger.debug("totalPlayers: {}", totalPlayers);
+        logger.debug("chunkSize: {}", chunkSize);
+
+        List<ImmutablePair<Integer, Integer>> result = new ArrayList<>();
+
+        if (chunkSize > 0) {
+            for (int i = 0; i < threadsCount; i++) {
+                int chunkMinPlayerId = minPlayerId + (chunkSize * i);
+                int chunkMaxPlayerId = chunkMinPlayerId + chunkSize - 1;
+
+                result.add(new ImmutablePair<>(chunkMinPlayerId, chunkMaxPlayerId));
+            }
+        }
+        else {
+            logger.warn("chunkSize = {}, probably totalPlayers = {} < threadsCount = {}. Only one chunk with all tasks will be returned.", chunkSize, totalPlayers, threadsCount);
+        }
+
+        if (totalPlayers % threadsCount != 0) { // add a "tail part" if required
+            int chunkMinPlayerId = minPlayerId + (chunkSize * threadsCount);
+            int chunkMaxPlayerId = maxPlayerId;
+
+            result.add(new ImmutablePair<>(chunkMinPlayerId, chunkMaxPlayerId));
+        }
+
+        return result;
     }
 }
