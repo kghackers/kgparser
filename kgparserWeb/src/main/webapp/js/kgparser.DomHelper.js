2dc132bd8397d2c70fa45a5847f92162698c816f
dmitry-weirdo <dmitry.weirdo@gmail.com>
2021-01-20 19:49:45 +0100

    chore: Sonar fixes for kgparser.DomHelper.js

--- kgparserWeb/src/main/webapp/js/kgparser.DomHelper.js
+++ kgparserWeb/src/main/webapp/js/kgparser.DomHelper.js
@@ -1,190 +1,200 @@
-//Ext.namespace('su.opencode');
+const DomHelper = function () {
+    const CLASS_NAMES_SEPARATOR = ' ';
 
-var DomHelper = function() {
-	var CLASS_NAMES_SEPARATOR = ' ';
+    return {
+        TAG_OPEN: '<',
+        TAG_CLOSE: '>',
 
-	return {
-		TAG_OPEN: '<',
-		TAG_CLOSE: '>',
+        BODY_TAG: 'body',
+        HEAD_TAG: 'head',
 
-		BODY_TAG: 'body',
-		HEAD_TAG: 'head',
+        SCRIPT_TAG: 'script',
+        LINK_TAG: 'link',
 
-		SCRIPT_TAG: 'script',
-		LINK_TAG: 'link',
+        DIV_TAG: 'div',
+        SPAN_TAG: 'span',
+        A_TAG: 'a',
 
-		DIV_TAG: 'div',
-		SPAN_TAG: 'span',
-		A_TAG: 'a',
+        BR_TAG: 'br',
 
-		BR_TAG: 'br',
+        INPUT_TAG: 'input',
+        LABEL_TAG: 'label',
 
-		INPUT_TAG: 'input',
-		LABEL_TAG: 'label',
+        H1_TAG: 'h1',
+        H2_TAG: 'h2',
+        H3_TAG: 'h3',
+        H4_TAG: 'h4',
 
-		H1_TAG: 'h1',
-		H2_TAG: 'h2',
-		H3_TAG: 'h3',
-		H4_TAG: 'h4',
+        TABLE_TAG: 'table',
+        TBODY_TAG: 'tbody',
+        TR_TAG: 'tr',
+        TH_TAG: 'th',
+        TD_TAG: 'td',
+        COL_GROUP_TAG: 'colgroup',
+        COL_TAG: 'col',
 
-		TABLE_TAG: 'table',
-		TBODY_TAG: 'tbody',
-		TR_TAG: 'tr',
-		TH_TAG: 'th',
-		TD_TAG: 'td',
-		COL_GROUP_TAG: 'colgroup',
-		COL_TAG: 'col',
+        IMG_TAG: 'img',
 
-		IMG_TAG: 'img',
+        UL_TAG: 'ul',
+        LI_TAG: 'li',
 
-		UL_TAG: 'ul',
-		LI_TAG: 'li',
+        ID_ATTRIBUTE: 'id',
+        CLASS_ATTRIBUTE: 'class',
+        STYLE_ATTRIBUTE: 'style',
+        TITLE_ATTRIBUTE: 'title',
+        FOR_ATTRIBUTE: 'for',
+        ROW_SPAN_ATTRIBUTE: 'rowspan',
+        COL_SPAN_ATTRIBUTE: 'colspan',
 
-		ID_ATTRIBUTE: 'id',
-		CLASS_ATTRIBUTE: 'class',
-		STYLE_ATTRIBUTE: 'style',
-		TITLE_ATTRIBUTE: 'title',
-		FOR_ATTRIBUTE: 'for',
-		ROW_SPAN_ATTRIBUTE: 'rowspan',
-		COL_SPAN_ATTRIBUTE: 'colspan',
+        SRC_ATTRIBUTE: 'src',
+        ALT_ATTRIBUTE: 'alt',
+        TYPE_ATTRIBUTE: 'type',
+        NAME_ATTRIBUTE: 'name',
+        VALUE_ATTRIBUTE: 'value',
+        WIDTH_ATTRIBUTE: 'width',
+        HEIGHT_ATTRIBUTE: 'height',
+        MAX_LENGTH_ATTRIBUTE: 'maxlength',
+        PATTERN_ATTRIBUTE: 'pattern',
+        CHARSET_ATTRIBUTE: 'charset',
+        REL_ATTRIBUTE: 'rel',
+        HREF_ATTRIBUTE: 'href',
+        TAB_INDEX_ATTRIBUTE: 'tabindex',
 
-		SRC_ATTRIBUTE: 'src',
-		ALT_ATTRIBUTE: 'alt',
-		TYPE_ATTRIBUTE: 'type',
-		NAME_ATTRIBUTE: 'name',
-		VALUE_ATTRIBUTE: 'value',
-		WIDTH_ATTRIBUTE: 'width',
-		HEIGHT_ATTRIBUTE: 'height',
-		MAX_LENGTH_ATTRIBUTE: 'maxlength',
-		PATTERN_ATTRIBUTE: 'pattern',
-		CHARSET_ATTRIBUTE: 'charset',
-		REL_ATTRIBUTE: 'rel',
-		HREF_ATTRIBUTE: 'href',
-		TAB_INDEX_ATTRIBUTE: 'tabindex',
+        HIDDEN_TYPE_ATTRIBUTE_VALUE: 'hidden',
+        TEXT_TYPE_ATTRIBUTE_VALUE: 'text',
+        CHECKBOX_TYPE_ATTRIBUTE_VALUE: 'checkbox',
+        RADIO_TYPE_ATTRIBUTE_VALUE: 'radio',
+        PASSWORD_TYPE_ATTRIBUTE_VALUE: 'password',
+        BUTTON_TYPE_ATTRIBUTE_VALUE: 'button',
 
-		HIDDEN_TYPE_ATTRIBUTE_VALUE: 'hidden',
-		TEXT_TYPE_ATTRIBUTE_VALUE: 'text',
-		CHECKBOX_TYPE_ATTRIBUTE_VALUE: 'checkbox',
-		RADIO_TYPE_ATTRIBUTE_VALUE: 'radio',
-		PASSWORD_TYPE_ATTRIBUTE_VALUE: 'password',
-		BUTTON_TYPE_ATTRIBUTE_VALUE: 'button',
+        getEl: function (id) {
+            return document.getElementById(id);
+        },
 
-		getEl: function(id) {
-			return document.getElementById(id);
-		},
+        createEl: function (tag, id, cls, text) {
+            const el = document.createElement(tag);
 
-		createEl: function(tag, id, cls, text) {
-			var el = document.createElement(tag);
+            if (id) {
+                el.id = id;
+            }
 
-			if (id)
-				el.id = id;
+            if (cls) {
+                const cssClass = DomHelper.getClass(cls);
+                if (cssClass) {
+                    el.className = cssClass;
+                }
+            }
 
-			if (cls)
-			{
-				var cssClass = DomHelper.getClass(cls);
-				if ( cssClass )
-					el.className = cssClass;
-			}
+            if (text) {
+                el.appendChild(document.createTextNode(text));
+            }
 
-			if (text)
-				el.appendChild(document.createTextNode(text));
+            return el;
+        },
 
-			return el;
-		},
+        createTN: function (text) {
+            return document.createTextNode(text);
+        },
 
-		createTN: function(text) {
-			return document.createTextNode(text);
-		},
+        createScriptEl: function (src) {
+            const scriptElement = document.createElement(DomHelper.SCRIPT_TAG);
+            scriptElement.setAttribute(DomHelper.TYPE_ATTRIBUTE, 'text/javascript');
+            scriptElement.setAttribute(DomHelper.SRC_ATTRIBUTE, src);
+            scriptElement.setAttribute(DomHelper.CHARSET_ATTRIBUTE, 'UTF-8');
 
-		createScriptEl: function(src) {
-			var scriptElement = document.createElement(DomHelper.SCRIPT_TAG);
-			scriptElement.setAttribute(DomHelper.TYPE_ATTRIBUTE, 'text/javascript');
-			scriptElement.setAttribute(DomHelper.SRC_ATTRIBUTE, src);
-			scriptElement.setAttribute(DomHelper.CHARSET_ATTRIBUTE, 'UTF-8');
+            return scriptElement;
+        },
 
-			return scriptElement;
-		},
+        createImg: function (src, alt) {
+            const imgElement = document.createElement(DomHelper.IMG_TAG);
+            imgElement.setAttribute(DomHelper.SRC_ATTRIBUTE, src);
+            imgElement.setAttribute(DomHelper.ALT_ATTRIBUTE, alt || '');
 
-		createImg: function(src, alt) {
-			var imgElement = document.createElement(DomHelper.IMG_TAG);
-			imgElement.setAttribute(DomHelper.SRC_ATTRIBUTE, src);
-			imgElement.setAttribute(DomHelper.ALT_ATTRIBUTE, alt || '');
+            return imgElement;
+        },
 
-			return imgElement;
-		},
+        appendCss: function (cssUrl, headId) {
+            if (!cssUrl) {
+                return;
+            }
 
-		appendCss: function(cssUrl, headId) {
-			if ( !cssUrl )
-				return;
+            const cssLink = document.createElement(DomHelper.LINK_TAG);
+            cssLink.setAttribute(DomHelper.REL_ATTRIBUTE, 'stylesheet');
+            cssLink.setAttribute(DomHelper.TYPE_ATTRIBUTE, 'text/css');
+            cssLink.setAttribute(DomHelper.HREF_ATTRIBUTE, cssUrl);
 
-			var cssLink = document.createElement(DomHelper.LINK_TAG);
-			cssLink.setAttribute(DomHelper.REL_ATTRIBUTE, 'stylesheet');
-			cssLink.setAttribute(DomHelper.TYPE_ATTRIBUTE, 'text/css');
-			cssLink.setAttribute(DomHelper.HREF_ATTRIBUTE, cssUrl);
+            let headElement = document.getElementsByTagName(DomHelper.HEAD_TAG)[0];
+            if (headId) {
+                headElement = document.getElementById(headId);
+            }
 
-			var headElement = document.getElementsByTagName(DomHelper.HEAD_TAG)[0];
-			if ( headId )
-				headElement = document.getElementById(headId);
+            if (!headElement) {
+                alert('No head element for append css link to');
+                return;
+            }
 
-			if ( !headElement )
-			{
-				alert('No head element for append css link to');
-				return;
-			}
+            headElement.appendChild(cssLink);
+        },
 
-			headElement.appendChild(cssLink);
-		},
+        insertAfter: function (nodeBefore, nodeToInsert) {
+            if (!nodeBefore) {
+                return;
+            }
 
-		insertAfter: function(nodeBefore, nodeToInsert) {
-			if ( !nodeBefore )
-				return;
+            const parentNode = nodeBefore.parentNode;
+            if (nodeBefore.nextSibling) {
+                parentNode.insertBefore(nodeToInsert, nodeBefore.nextSibling); // append before next sibling
+            }
+            else {
+                parentNode.appendChild(nodeToInsert); // append last
+            }
+        },
+        insertBefore: function (parentNode, nodeAfter, nodeToInsert) {
+            if (!nodeAfter) {
+                parentNode.appendChild(nodeToInsert); // append last
+            }
+            else {
+                parentNode.insertBefore(nodeToInsert, nodeAfter);
+            }
+        },
 
-			var parentNode = nodeBefore.parentNode;
-			if (nodeBefore.nextSibling)
-				parentNode.insertBefore(nodeToInsert, nodeBefore.nextSibling); // append before next sibling
-			else
-				parentNode.appendChild(nodeToInsert); // append last
-		},
-		insertBefore: function(parentNode, nodeAfter, nodeToInsert) {
-			if ( !nodeAfter )
-				parentNode.appendChild(nodeToInsert); // append last
-			else
-				parentNode.insertBefore(nodeToInsert, nodeAfter);
-		},
+        getClass: function (classNames) {
+            if (!classNames) {
+                return null;
+            }
 
-		getClass: function(classNames) {
-			if (!classNames)
-				return null;
+            if (!(classNames instanceof Array)) {
+                return classNames;
+            }
 
-			if ( !(classNames instanceof Array ) )
-				return classNames;
+            let className = '';
+            for (let item of classNames) {
+                className += (item + CLASS_NAMES_SEPARATOR);
+            }
 
-			var className = '';
-			for (var i = 0; i < classNames.length; i++)
-				className += (classNames[i] + CLASS_NAMES_SEPARATOR);
+            className = className.substring(0, (className.length - CLASS_NAMES_SEPARATOR.length)); // remove trailing space
+            return className;
+        },
 
-			className = className.substring( 0, (className.length - CLASS_NAMES_SEPARATOR.length) ); // remove trailing space
-			return className;
-		},
+        removeChildren: function (element) {
+            while (element.hasChildNodes()) {
+                element.removeChild(element.lastChild);
+            }
+        },
 
-		removeChildren: function(element) {
-			while (element.hasChildNodes()) {
-				element.removeChild(element.lastChild);
-			}
-		},
+        removeChildrenHierarchy: function (element) {
+            while (element.hasChildNodes()) {
+                DomHelper.removeChildrenHierarchy(element.lastChild);
+                element.removeChild(element.lastChild);
+            }
+        },
 
-		removeChildrenHierarchy: function(element) {
-			while (element.hasChildNodes()) {
-				DomHelper.removeChildrenHierarchy(element.lastChild);
-				element.removeChild(element.lastChild);
-			}
-		},
+        removeNbsps: function (str) {
+            if (!str || !str.length) {
+                return '';
+            }
 
-		removeNbsps: function(str) {
-			if ( !str || !str.length )
-				return '';
-
-			return str.replace('&nbsp;', ' ');
-		}
-	}
+            return str.replace('&nbsp;', ' ');
+        }
+    }
 }();
