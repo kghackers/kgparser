bdd319419205ec89df16b0520cf05d4fe7e95dbb
dmitry-weirdo <dmitry.weirdo@gmail.com>
2021-01-23 00:49:38 +0100

    feat: Added copying statics (css, js, old top) to the generated statistics directory. - Current statics are copied, including the top pages from 2012-12-09 generation. This has to be changed in the future. - Statics are copied from the source directory.

--- run-statistics-process.sh
+++ run-statistics-process.sh
@@ -1,7 +1,9 @@
 # declare required variables
 # todo: we should be able to override them from the command line
 LOG4J_XML_FILE_PATH=log4j2.xml
-KGSTATS_JAR_FILE_PATH=kgstatsSrv/target/kgstats-srv-1.0.jar
+KGSTATS_SRV_JAR_FILE_PATH=kgstatsSrv/target/kgstats-srv-1.0.jar
+# todo: maybe take kgstatsWeb statics from target?
+KGSTATS_WEB_ROOT_DIR=kgstatsWeb/src/main/webapp
 SPRING_CONFIG_LOCATION=kgstatsSrv/src/main/resources/application.actions.properties
 DATABASE_USER=root
 DATABASE_PASSWORD=root
@@ -9,12 +11,13 @@ DATABASE_NAME=actions
 INPUT_CONFIG_FILE_NAME=/d/kg/config.json
 OUTPUT_CONFIG_FILE_NAME=/d/kg/config-output.json
 GENERATE_STATISTICS_DIRECTORY=/d/kg/stats/
+STATIC_DIR=$GENERATE_STATISTICS_DIRECTORY
 
-# download statistics from Klavogonki to JSON files
+## download statistics from Klavogonki to JSON files
 java \
 -Dfile.encoding=UTF8 \
 -Dlog4j.configurationFile=$LOG4J_XML_FILE_PATH \
--jar $KGSTATS_JAR_FILE_PATH \
+-jar $KGSTATS_SRV_JAR_FILE_PATH \
 DOWNLOAD_PLAYER_DATA \
 $INPUT_CONFIG_FILE_NAME \
 $OUTPUT_CONFIG_FILE_NAME
@@ -33,17 +36,17 @@ java \
 -Dlog4j.configurationFile=$LOG4J_XML_FILE_PATH \
 -Dspring.profiles.active=database \
 -Dspring.config.location=$SPRING_CONFIG_LOCATION \
--jar $KGSTATS_JAR_FILE_PATH \
+-jar $KGSTATS_SRV_JAR_FILE_PATH \
 IMPORT_JSON_TO_DATABASE \
 $OUTPUT_CONFIG_FILE_NAME
 
 # add indices to the database
+# todo: implement
 
 # generate statistics from database
-
-mkdir $GENERATE_STATISTICS_DIRECTORY
-mkdir $GENERATE_STATISTICS_DIRECTORY/js
-mkdir $GENERATE_STATISTICS_DIRECTORY/xlsx
+mkdir -p $GENERATE_STATISTICS_DIRECTORY
+mkdir -p $GENERATE_STATISTICS_DIRECTORY/js
+mkdir -p $GENERATE_STATISTICS_DIRECTORY/xlsx
 
 # spring.config.location can contain file path or the directory (directory should end with /)
 # see https://docs.spring.io/spring-boot/docs/1.0.1.RELEASE/reference/html/boot-features-external-config.html#boot-features-external-config-application-property-files
@@ -52,16 +55,30 @@ java \
 -Dlog4j.configurationFile=$LOG4J_XML_FILE_PATH \
 -Dspring.profiles.active=database \
 -Dspring.config.location=$SPRING_CONFIG_LOCATION \
--jar $KGSTATS_JAR_FILE_PATH \
+-jar $KGSTATS_SRV_JAR_FILE_PATH \
 GENERATE_STATISTICS_FROM_DATABASE \
 $OUTPUT_CONFIG_FILE_NAME
 
 # copy static files from sources of kgstatsWeb to generated statistics
-# todo: copy ignore html, zip and xlsx
+cp -R $KGSTATS_WEB_ROOT_DIR/css $STATIC_DIR
+cp -R $KGSTATS_WEB_ROOT_DIR/img $STATIC_DIR
+
+# todo: after we exclude the generated files from sources, copy js recursively
+# cp -R $KGSTATS_WEB_ROOT_DIR/js $STATIC_DIR
+cp $KGSTATS_WEB_ROOT_DIR/js/players-by-rank-chart.js $STATIC_DIR/js
+cp $KGSTATS_WEB_ROOT_DIR/js/stats-data.js $STATIC_DIR/js
+cp $KGSTATS_WEB_ROOT_DIR/js/stats-top-table.js $STATIC_DIR/js
+
+# todo: old tops must be copied from archive S3 buckets, not from sources
+cp -R $KGSTATS_WEB_ROOT_DIR/2020-12-09 $STATIC_DIR
+
+echo "Copied static files from $KGSTATS_WEB_ROOT_DIR to $STATIC_DIR."
 
 # zip the statistics site
+# todo: implement
 
 # create database dump
+# todo: implement
 
 # drop MySQL database
 mysql \
