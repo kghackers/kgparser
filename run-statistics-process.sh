32c7614f70807e27f05dcc4b8cda423f3f1f1460
dmitry-weirdo <dmitry.weirdo@gmail.com>
2021-01-23 03:09:51 +0100

    feat: added zipping of json files of the current data load to json.zip.

--- run-statistics-process.sh
+++ run-statistics-process.sh
@@ -1,13 +1,16 @@
 # declare required variables
 # todo: we should be able to override them from the command line
 ROOT_WORKING_DIR=/d/kg/
+
 LOG4J_XML_FILE_PATH=log4j2.xml
+
 KGSTATS_SRV_JAR_FILE_PATH=kgstatsSrv/target/kgstats-srv-1.0.jar
 # todo: maybe take kgstatsSrv statics from target?
 KGSTATS_SRV_SQL_DIR=kgstatsSrv/src/main/resources/sql
 # todo: maybe take kgstatsWeb statics from target?
 KGSTATS_WEB_ROOT_DIR=kgstatsWeb/src/main/webapp
 SPRING_CONFIG_LOCATION=kgstatsSrv/src/main/resources/application.actions.properties
+
 DATABASE_USER=root
 DATABASE_PASSWORD=root
 DATABASE_NAME=actions
@@ -15,14 +18,22 @@ DATABASE_NAME=actions
 DATABASE_DUMP_FILE_NAME=actions.sql
 DATABASE_DUMP_FILE_PATH=$ROOT_WORKING_DIR/$DATABASE_DUMP_FILE_NAME
 
-INPUT_CONFIG_FILE_NAME=/d/kg/config.json
-OUTPUT_CONFIG_FILE_NAME=/d/kg/config-output.json
+JSON_FILES_DIRECTORY_NAME=/json
+JSON_FILES_DIRECTORY_PATH=$ROOT_WORKING_DIR/$JSON_FILES_DIRECTORY_NAME
+
+# todo: generate input config file by given data in the $ROOT_WORKING_DIR
+INPUT_CONFIG_FILE_NAME=config.json
+INPUT_CONFIG_FILE_PATH=$ROOT_WORKING_DIR/$INPUT_CONFIG_FILE_NAME
+
+OUTPUT_CONFIG_FILE_NAME=config-output.json
+OUTPUT_CONFIG_FILE_PATH=$ROOT_WORKING_DIR/$OUTPUT_CONFIG_FILE_NAME
 
 GENERATE_STATISTICS_DIRECTORY_NAME=/stats
 GENERATE_STATISTICS_DIRECTORY_PATH=$ROOT_WORKING_DIR/$GENERATE_STATISTICS_DIRECTORY_NAME
 STATIC_DIR=$GENERATE_STATISTICS_DIRECTORY_PATH
 
 # zip files
+JSON_DATA_ZIP_FILE_PATH=$ROOT_WORKING_DIR/json.zip
 STATISTICS_ZIP_FILE_PATH=$ROOT_WORKING_DIR/statistics.zip
 DATABASE_DUMP_ZIP_FILE_PATH=$ROOT_WORKING_DIR/database.zip
 
@@ -32,13 +43,13 @@ java \
 -Dlog4j.configurationFile=$LOG4J_XML_FILE_PATH \
 -jar $KGSTATS_SRV_JAR_FILE_PATH \
 DOWNLOAD_PLAYER_DATA \
-$INPUT_CONFIG_FILE_NAME \
-$OUTPUT_CONFIG_FILE_NAME
+$INPUT_CONFIG_FILE_PATH \
+$OUTPUT_CONFIG_FILE_PATH
 
 echo "Downloaded players data from Klavogonki."
 
-echo "Output file $OUTPUT_CONFIG_FILE_NAME:"
-cat $OUTPUT_CONFIG_FILE_NAME
+echo "Output file $OUTPUT_CONFIG_FILE_PATH:"
+cat $OUTPUT_CONFIG_FILE_PATH
 
 ## create MySQL database
 mysql \
@@ -58,7 +69,7 @@ java \
 -Dspring.config.location=$SPRING_CONFIG_LOCATION \
 -jar $KGSTATS_SRV_JAR_FILE_PATH \
 IMPORT_JSON_TO_DATABASE \
-$OUTPUT_CONFIG_FILE_NAME
+$OUTPUT_CONFIG_FILE_PATH
 
 # add indices to the database
 ADD_INDEXES_FILE_PATH=$KGSTATS_SRV_SQL_DIR/add-indexes.sql
@@ -85,7 +96,7 @@ java \
 -Dspring.config.location=$SPRING_CONFIG_LOCATION \
 -jar $KGSTATS_SRV_JAR_FILE_PATH \
 GENERATE_STATISTICS_FROM_DATABASE \
-$OUTPUT_CONFIG_FILE_NAME
+$OUTPUT_CONFIG_FILE_PATH
 
 echo "Generated statistics files from database $DATABASE_NAME to directory $GENERATE_STATISTICS_DIRECTORY_PATH."
 
@@ -109,13 +120,27 @@ mysqldump -u$DATABASE_USER -p$DATABASE_PASSWORD $DATABASE_NAME > $DATABASE_DUMP_
 
 echo "Dumped database $DATABASE_NAME to file $DATABASE_DUMP_FILE_PATH."
 
+# parse data download start date/time from output config file
+DATA_DOWNLOAD_START_DATE=$(jq -r .dataDownloadStartDateString $OUTPUT_CONFIG_FILE_PATH)
+
+echo "Parsed data download start date $DATA_DOWNLOAD_START_DATE from output config file $OUTPUT_CONFIG_FILE_PATH."
+
+# zip uses the relative paths, therefore we have to navigate to root directory
+cd $JSON_FILES_DIRECTORY_PATH || exit
+
+echo "Change to root working directory $JSON_FILES_DIRECTORY_PATH."
+
+# zip the json files
+JSON_FILES_CURRENT_DOWNLOAD_DIRECTORY_PATH=$JSON_FILES_DIRECTORY_PATH/$DATA_DOWNLOAD_START_DATE
+
+zip -r $JSON_DATA_ZIP_FILE_PATH ./"$DATA_DOWNLOAD_START_DATE"
+
+echo "Zipped directory $JSON_FILES_CURRENT_DOWNLOAD_DIRECTORY_PATH to zip file $JSON_DATA_ZIP_FILE_PATH."
+
 # zip uses the relative paths, therefore we have to navigate to root directory
 cd $ROOT_WORKING_DIR || exit
 
 echo "Change to root working directory $ROOT_WORKING_DIR."
-
-# zip the json files
-# todo: implement
 
 # zip the statistics site
 zip -r $STATISTICS_ZIP_FILE_PATH ./$GENERATE_STATISTICS_DIRECTORY_NAME
