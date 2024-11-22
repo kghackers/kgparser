38e9f67aa15f97d69cb1e02c74a2dfb266ac938d
dmitry-weirdo <dmitry.weirdo@gmail.com>
2024-11-22 03:03:06 +0100

    feat: #81 Convert HaulUtils and HaulUtilsTest to Kotlin.

---
+++ kgstatsSrv/src/main/kotlin/ru/klavogonki/statistics/freemarker/HaulUtils.kt
@@ -0,0 +1,30 @@
+package ru.klavogonki.statistics.freemarker
+
+import org.apache.logging.log4j.kotlin.Logging
+
+object HaulUtils : Logging {
+    // todo: use some library with these constants
+    // see https://stackoverflow.com/questions/2442486/time-consts-in-java
+    private const val SECONDS_IN_MINUTE = 60
+    private const val MINUTES_IN_HOUR = 60
+    private const val SECONDS_IN_HOUR = SECONDS_IN_MINUTE * MINUTES_IN_HOUR
+
+    const val NO_HAUL_STRING = "—"
+
+    @JvmStatic
+    fun format(haul: Int?): String {
+        if (haul == null || haul < 0) {
+            return NO_HAUL_STRING
+        }
+
+        val hours = haul / SECONDS_IN_HOUR
+        val minutes = (haul % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE
+        val seconds = haul % SECONDS_IN_MINUTE
+
+        // todo: библиотека со склонением слов
+        val result = "$hours ч. $minutes мин. $seconds сек."
+        logger.debug("Haul $haul converted to formatted string \"$result\".")
+
+        return result
+    }
+}
